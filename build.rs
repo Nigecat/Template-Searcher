1743f815ddeab20e20ac926643fe4e0592da675f
Nigecat <nigel@nigelq.com>
2020-08-25 14:11:14 +1000

    Add custom application icon

---
+++ build.rs
@@ -0,0 +1,12 @@
+#[cfg(windows)]
+use winres;
+
+#[cfg(windows)]
+fn main() {
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("resources/icon.ico");
+    res.compile().unwrap();
+}
+
+#[cfg(unix)]
+fn main() { }
--- src/searcher.rs
+++ src/searcher.rs
@@ -2,12 +2,14 @@ use web_view::*;
 use snailquote::escape;
 
 pub fn start_search() -> () {
+    println!("starting search");
+
     web_view::builder()
         .title(crate::NAME)
         .content(Content::Html(include_str!("display.html")))
         .size(852, 480)
         .resizable(false)
-        .debug(true)
+        .frameless(true)
         .user_data(())
         .invoke_handler(invoke_handler)
         .run()
@@ -18,8 +20,8 @@ fn invoke_handler(webview: &mut WebView<()>, arg: &str) -> WVResult {
     println!("recieved arg: {}", arg);
 
     if arg == "init" {
+        webview.eval(&format!("window.searchDir = {}", escape(&crate::config::get("path")))).expect("unable to init window");
         webview.set_fullscreen(true);
-        webview.eval(&format!("setup({})", escape(&crate::config::get("path")))).expect("unable to init window");
     }
 
     else if arg == "exit" {
