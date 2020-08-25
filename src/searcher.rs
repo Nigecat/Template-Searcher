6c12f8374d286433d783770bcbc8abe942625e29
Nigecat <nigel@nigelq.com>
2020-08-25 12:51:18 +1000

    Bump webview

--- src/searcher.rs
+++ src/searcher.rs
@@ -2,15 +2,27 @@ use web_view::*;
 
 pub fn start_search(path: String) -> () {
     println!("starting search at {}", path);
-	
+    
     web_view::builder()
         .title(crate::NAME)
         .content(Content::Html(include!("display.rs")))
-        .size(320, 480)
+        .size(852, 480)
         .resizable(false)
         .debug(true)
         .user_data(())
-        .invoke_handler(|_webview, _arg| Ok(()))
+        .invoke_handler(invoke_handler)
         .run()
         .unwrap();
+}
+
+fn invoke_handler(webview: &mut WebView<()>, arg: &str) -> WVResult {
+    if arg == "init" {
+        webview.set_fullscreen(true);
+    }
+
+    else if arg == "exit" {
+        webview.exit();
+    }
+
+    Ok(())
 }
