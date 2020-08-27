fbdceee3cc89ad5fd53e28b1458833fbcd9177ac
Nigecat <nigel@nigelq.com>
2020-08-27 11:33:20 +1000

    Add config file

--- Makefile
+++ Makefile
@@ -1,10 +1,18 @@
+.PHONE: build clean
 
-build: pre-build obj/resources.res
-	g++ src/main.cpp -o bin/Template\ Searcher.exe obj/resources.res
+build: pre-build obj/resources.res bin/config.ini
+	g++ -std=c++17 src/main.cpp -o bin/Template\ Searcher.exe obj/resources.res
 
 pre-build:
 	mkdir -p bin
 	mkdir -p obj
 
-obj/resources.res:
+bin/config.ini: src/config.ini
+	cp src/config.ini bin/config.ini
+
+obj/resources.res: src/resources.rc
 	windres src/resources.rc -O coff -o obj/resources.res
+
+clean:
+	rm -rf bin
+	rm -rf obj
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
-#include <cstdio>
+#undef UNICODE
+#include <iostream>
+#include <windows.h>
+#include <filesystem>
+#include "../lib/INIReader.h"
+
+namespace fs = std::filesystem;
 
 int main() {
-    printf("Hello World");
+    // Assume our config file is in the same directory as the application
+    char pathBuf[MAX_PATH];
+    GetModuleFileName(NULL, pathBuf, sizeof(pathBuf));
+    fs::path tempPath(pathBuf);
+
+    INIReader config((tempPath.parent_path() / "config.ini").string());
+    std::string shortcut = config.Get("config", "shortcut", "");
+    fs::path path(config.Get("config", "path", ""));
+
+
+    return 0;
 }
